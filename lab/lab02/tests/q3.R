64811c857eef807ed26463b191bec053e167af5d
Iris Yen <irisyen@Iriss-MacBook-Air.local>
2022-08-30 23:10:23 -0700

    otter update

--- lab/lab02/tests/q3.R
+++ lab/lab02/tests/q3.R
@@ -4,7 +4,7 @@ test = list(
     ottr::TestCase$new(
       hidden = FALSE,
       name = NA,
-      points = 1.0,
+      points = 1,
       code = {
         test_that("p3a", {
           expect_true(is.data.frame(CS_data))
@@ -15,7 +15,7 @@ test = list(
     ottr::TestCase$new(
       hidden = FALSE,
       name = NA,
-      points = 1.0,
+      points = 1,
       code = {
         test_that("p3b", {
           expect_true(nrow(CS_data) == 137 && ncol(CS_data) == 9)
@@ -26,7 +26,7 @@ test = list(
     ottr::TestCase$new(
       hidden = FALSE,
       name = NA,
-      points = 1.0,
+      points = 1,
       code = {
         test_that("p3c", {
           expect_true(all.equal(CS_data$CS_rate[1] * 100, CS_data$CS_rate_100[1]))
